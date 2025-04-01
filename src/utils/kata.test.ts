4feaae4d7d4c230cc5cbcc6cb3170db8d358dbed
vikrant78951 <vikrant78951@gmail.com>
2025-04-01 20:23:50 +0530

    completed kata test cases

--- src/utils/kata.test.ts
+++ src/utils/kata.test.ts
@@ -1,6 +1,10 @@
-import { add } from "./kata";
+import { add, getCalledCount, resetCallCount } from "./kata";
 
 describe("test string calculater kata", () => {
+  beforeEach(() => {
+    resetCallCount();
+  });
+
   let res;
 
   test("return 0 for empty string", () => {
@@ -23,15 +27,32 @@ describe("test string calculater kata", () => {
     expect(res).toBe(6);
   });
 
-  test.only("should throw error for negative number ", () => {
+  test("should handle multiple delimiters with length longer than one char  ", () => {
+    res = add("//[;;][**]\n1;2*3");
+    expect(res).toBe(6);
+  });
+
+  test("should throw error for negative number ", () => {
     expect(() => add("//[;][*]\n1;-2*3")).toThrow(
       "Negative numbers are not allowed: -2",
     );
   });
 
-  test.only("throws an error for multiple negative numbers", () => {
+  test("throws an error for multiple negative numbers", () => {
     expect(() => add("-1,-2,-3")).toThrow(
       "Negative numbers are not allowed: -1, -2, -3",
     );
+  });
+
+  test("increments count with each call", () => {
+    add("1");
+    add("2");
+    add("3");
+    expect(getCalledCount()).toBe(3);
+  });
+
+  test("ignore number bigger then 1000", () => {
+    res = add("1,2,1001");
+    expect(res).toBe(3);
   });
 });
--- src/utils/kata.ts
+++ src/utils/kata.ts
@@ -1,6 +1,9 @@
-// add function
+// global variables
+let callCount = 0;
 
+// add function
 export function add(numbers: string): number {
+  callCount++;
   if (numbers === "") return 0;
   let delimiters = [",", "\n"];
   if (numbers.startsWith("//")) {
@@ -13,18 +16,26 @@ export function add(numbers: string): number {
 
   const delimiterRegex = new RegExp(`[${delimiters.join("")}]`, "g");
 
-  const numArray = numbers.split(delimiterRegex).map(Number);
+  let numArray = numbers.split(delimiterRegex).map(Number);
 
   // validate negative  number
   const negatives = numArray.filter((num) => num < 0);
   if (negatives.length > 0) {
-    throw new Error(
-      `Negative numbers are not allowed: ${negatives.join(", ")}`,
-    );
+    throw new Error(`Negative numbers are not allowed: ${negatives.join(",")}`);
   }
+
+  // ignore number bigger then 1000
+  numArray = numArray.filter((num) => num < 1001);
 
   const sum: number = numArray.reduce((sum, num) => sum + num, 0);
   return sum;
 }
 
-add("//[;][*]\n1;2*3");
+export function getCalledCount(): number {
+  return callCount;
+}
+export function resetCallCount() {
+  callCount = 0;
+}
+
+add("1,2,1000");
